5e2dd20137f54a141290db61071d5ce4edf93e9b
hamburger <74403054+hong770@users.noreply.github.com>
2021-03-30 19:50:21 +0800

    Add files via upload

---
+++ 2HW.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class _2HW : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int n = 10;
+            int num = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % 2 == 0) || (i % 3 == 0))
+                {
+                    num = num + i;
+                }
+            }
+            Response.Write(num);
+        }
+    }
+    }
